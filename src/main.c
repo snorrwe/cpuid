b56fea1c5503c891e81dcf1d9f1690c9f06c4912
Daniel Kiss <littlesnorrboy@gmail.com>
2021-07-27 00:11:07 +0200

    Swap cache and info

--- src/main.c
+++ src/main.c
@@ -4,6 +4,8 @@
 
 int main(void)
 {
+    puts("\n============================== Processor Info ===============================\n");
+    cq_cpuinfo();
     puts("\n============================= Cache Information =============================\n");
     unsigned cache_idx = 0;
     while (1) {
@@ -14,8 +16,6 @@ int main(void)
         cq_print_cache_info(stdout, &info);
         ++cache_idx;
     }
-    puts("\n============================== Processor Info ===============================\n");
-    cq_cpuinfo();
     puts("\n=========================== Extension Information ===========================\n");
     cq_extensions();
 }
